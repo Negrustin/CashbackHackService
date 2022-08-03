82743f8f29e33d235181f716c410d90655f8e626
Aliaksandr Zuyeuski <negrustin87@gmail.com>
2022-08-03 15:29:36 +0200

    add tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,35 @@
 package ru.netology.service;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void purchaseSumLessBonus() {
+        assertEquals(600,
+        service.remain(400));
+
+    }
+
+    @Test
+    public void purchaseSumEqualBonus() {
+        assertEquals(0,
+        service.remain(1000));
+    }
+
+    @Test
+    public void purchaseSumMultipleBonus(){
+        assertEquals(0,
+        service.remain(1000));
+    }
+
+
+
 }
+
+
+
