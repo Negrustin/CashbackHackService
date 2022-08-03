25963a04a1fa61c546fc177fce737b1e3c02e175
Aliaksandr Zuyeuski <negrustin87@gmail.com>
2022-08-03 17:15:28 +0200

    Adding tests at the boundary of equivalents

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,23 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
+    @Test
+    public void purchaseSumLowerBoundaryBonus(){
+
+        int expected = 999;
+        int actual = service.remain(1);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void purchaseSumUpperBoundaryBonus(){
+
+        int expected = 1;
+        int actual = service.remain(999);
+
+        assertEquals(expected,actual);
+    }
 
     @Test
     public void purchaseSumLessBonus() {
@@ -14,6 +31,7 @@ public class CashbackHackServiceTest {
         service.remain(400));
 
     }
+
 
     @Test
     public void purchaseSumEqualBonus() {
@@ -24,10 +42,8 @@ public class CashbackHackServiceTest {
     @Test
     public void purchaseSumMultipleBonus(){
         assertEquals(0,
-        service.remain(1000));
+        service.remain(2000));
     }
-
-
 
 }
 
